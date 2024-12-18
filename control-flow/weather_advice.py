bca868fec5ae012efa0e26ee343fc6af2f257702
ashup21 <ashenafipaul21@gmail.com>
2024-12-18 19:35:00 +0300

    updated

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,4 +1,4 @@
-current_weather = input("What's the weather like today? (sunny/rainy/cold):")
+current_weather = input("What's the weather like today? (sunny/rainy/cold):").lower()
 
 if current_weather == "sunny":
     print("Wear a t-shirt and sunglasses.")
@@ -8,4 +8,3 @@ elif current_weather == "cold":
     print("Make sure to wear a warm coat and a scarf.")
 else:
     print("Sorry, I don't have recommendations for this weather.")
-    
