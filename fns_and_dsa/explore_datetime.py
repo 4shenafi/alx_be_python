fb05169105e42647be48a86057cafdf1f10bb344
ashup21 <ashenafipaul21@gmail.com>
2025-01-09 14:41:46 +0300

    fix: update future date display format to show only the date

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -6,7 +6,7 @@ def display_current_datetime():
 def calculate_future_date():
     number_of_days = int(input("Enter the number of days to add to the current date: "))
     new_datetime = datetime.now() + timedelta(days=number_of_days)
-    print(f'Future date: {new_datetime.strftime("%Y-%m-%d %H:%M:%S")}')
+    print(f'Future date: {new_datetime.strftime("%Y-%m-%d")}')
 
 current_datetime = display_current_datetime()
 future_date = calculate_future_date()
